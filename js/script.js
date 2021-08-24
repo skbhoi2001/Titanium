21db10daae3d1ebc8a01ab8698b238ef3c4c6e64
skbhoi2001 <soumyak.bhoi.2001@gmail.com>
2021-08-24 09:33:04 +0530

    Updated

--- js/script.js
+++ js/script.js
@@ -59,21 +59,3 @@ anibutton4.addEventListener("mouseout",function(){
 
 
 //pricing
-function check(){
-  var checkBox = document.getElementById("checkbox");
-  var text1 = document.getElementsByClassName("text1");
-  var text2 = document.getElementsByClassName("text2");
-
-  for (var i=0; i < text1.length; i++){
-      if(checkBox.checked == true){
-          text1[i].style.display = "none";
-          text2[i].style.display = "block";
-      }
-      else if (checkBox.checked == false){
-          text1[i].style.display = "block";
-          text2[i].style.display = "none";
-      }
-  }
-}
-
-check();
